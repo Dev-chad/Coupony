52b6b34a9d9e716f07509e4dc99d72d034e1949c
kccistc <jieun7895@naver.com>
2019-05-08 19:04:57 +0900

    SIGN UP->hide, find password, reset password

---
+++ app/src/main/java/com/example/coupony/FindPassword.java
@@ -0,0 +1,13 @@
+package com.example.coupony;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FindPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_password);
+    }
+}
--- app/src/main/java/com/example/coupony/SignUpActivity.java
+++ app/src/main/java/com/example/coupony/SignUpActivity.java
@@ -2,6 +2,9 @@ package com.example.coupony;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        final Button button = findViewById(R.id.btn_hide);
+        final EditText editText = findViewById(R.id.edit_password);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(editText.getInputType() == 1){
+                    editText.setInputType(0x81);
+                    button.setText("HIDE");
+                }else {
+                    editText.setInputType(1);
+                    button.setText("SHOW");
+                }
+            }
+        });
     }
 }
