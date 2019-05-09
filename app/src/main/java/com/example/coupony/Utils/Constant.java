9bff144b854408937c18669d9b5cd071e330d77c
chad <dev.chadlim@gmail.com>
2019-05-09 19:51:57 +0900

    Http connection

---
+++ app/src/main/java/com/example/coupony/Utils/Constant.java
@@ -0,0 +1,7 @@
+package com.example.coupony.Utils;
+
+public class Constant {
+    final public static String SERVER_ADDRESS = "http://192.168.1.67";
+    final public static String SERVER_LOGIN_ADDRESS = SERVER_ADDRESS+"/login.php";
+    final public static String SERVER_SIGNUP_ADDRESS = SERVER_ADDRESS+"/signup.php";
+}
