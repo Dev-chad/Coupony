17593223c84fb7887cc78ee1bf2c6795297a3055
kccistc <jieun7895@naver.com>
2019-05-09 19:52:48 +0900

    화면전환

--- app/src/main/java/com/example/coupony/SignInActivity.java
+++ app/src/main/java/com/example/coupony/SignInActivity.java
@@ -10,6 +10,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.coupony.Utils.Encrypt;
+import com.example.coupony.Utils.ShopCategory;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -21,7 +22,6 @@ public class SignInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
-
         findViewById(R.id.btn_signin).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -30,13 +30,33 @@ public class SignInActivity extends AppCompatActivity {
                 String str1 = editText1.getText().toString();
                 String str2 = editText2.getText().toString();
 
-                Toast.makeText(SignInActivity.this, Encrypt.getSHA256(str2), Toast.LENGTH_LONG).show();
+                //Toast.makeText(SignInActivity.this, Encrypt.getSHA256(str2), Toast.LENGTH_LONG).show();
 
                 if (str1.equals(id) && str2.equals(password)) {
                     Toast.makeText(SignInActivity.this, "같아요", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(SignInActivity.this, ShopCategory.class);
+                    startActivity(intent);
+                    finish();
                 } else {
                     Toast.makeText(SignInActivity.this, "달라요", Toast.LENGTH_LONG).show();
                 }
+            }
+        });
+
+        findViewById(R.id.view_signup).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignInActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.view_password).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignInActivity.this, FindPassword.class);
+                startActivity(intent);
+
             }
         });
 
