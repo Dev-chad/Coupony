a42cb8aa60d3f06a38279734c01e4597976a95d7
kccistc <jieun7895@naver.com>
2019-05-14 21:52:10 +0900

    UserPage2

--- app/src/main/java/com/example/coupony/Activity/UserPageActivity.java
+++ app/src/main/java/com/example/coupony/Activity/UserPageActivity.java
@@ -9,6 +9,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.example.coupony.Data.User;
 import com.example.coupony.R;
 
 public class UserPageActivity extends AppCompatActivity {
@@ -19,10 +20,17 @@ public class UserPageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_page);
 
-        ListView userlistView= findViewById(R.id.list_user);
+        Intent intent = getIntent();
+        User user = intent.getParcelableExtra("user");
+
+        ListView userlistView = findViewById(R.id.list_user);
         ListView ownerlistView = findViewById(R.id.list_owner);
 
-        ownerlistView.setVisibility(View.INVISIBLE);
+        if (user.isbOwner()) {
+            ownerlistView.setVisibility(View.VISIBLE);
+        } else {
+            ownerlistView.setVisibility(View.INVISIBLE);
+        }
 
         final String[] userlist = {"개인 정보", "비밀번호 변경", "쿠폰함", "즐겨찾는 매장", "로그아웃"};
         final String[] ownerlist = {"매장 정보", "쿠폰 발급하기", "정보 수정 요청", "임시 운영 중지"};
@@ -37,10 +45,19 @@ public class UserPageActivity extends AppCompatActivity {
                         this, android.R.layout.simple_list_item_1, ownerlist);
         ownerlistView.setAdapter(owneradapter);
 
+
         userlistView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String title = userlist[position];
+                Intent intent = new Intent(UserPageActivity.this, ShopListActivity.class);      // ShopListActivity.class 바꾸기!
+                startActivity(intent);
+            }
+        });
+        ownerlistView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String title = ownerlist[position];
                 Intent intent = new Intent(UserPageActivity.this, ShopListActivity.class);      // ShopListActivity.class 바꾸기!
                 startActivity(intent);
             }
