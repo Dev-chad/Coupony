c92d55162bd4044fec2ca1e711c859d71a78cd15
chad <dev.chadlim@gmail.com>
2019-05-14 09:04:03 +0900

    Shop list view

--- app/src/main/java/com/example/coupony/Utils/Constant.java
+++ app/src/main/java/com/example/coupony/Utils/Constant.java
@@ -1,8 +1,8 @@
 package com.example.coupony.Utils;
 
 public class Constant {
-    //final public static String SERVER_ADDRESS = "http://192.168.1.67";
-    final public static String SERVER_ADDRESS = "http://59.16.96.120";
+    final public static String SERVER_ADDRESS = "http://192.168.1.67";
+    //final public static String SERVER_ADDRESS = "http://59.16.96.120";
     final public static String SERVER_LOGIN_ADDRESS = SERVER_ADDRESS+"/signIn.php";
     final public static String SERVER_SIGNUP_ADDRESS = SERVER_ADDRESS+"/signUp.php";
     final public static String SERVER_GETSHOPLIST_ADDRESS = SERVER_ADDRESS+"/getShopList.php";
