a1780d14836af14038df9997ccd4bc5bbe9d8984
chad <dev.chadlim@gmail.com>
2019-05-16 09:39:05 +0900

    dd

--- app/src/main/java/com/example/coupony/Activity/CouponListActivity.java
+++ app/src/main/java/com/example/coupony/Activity/CouponListActivity.java
@@ -1,5 +1,7 @@
 package com.example.coupony.Activity;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,11 +12,13 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.coupony.Adapter.CouponListAdapter;
 import com.example.coupony.Connect.HttpConnect;
 import com.example.coupony.Connect.RequestCallback;
@@ -36,6 +40,7 @@ public class CouponListActivity extends AppCompatActivity {
 
     private User user;
     private Shop shop;
+    private Coupon savingCoupon;
     private ArrayList<Coupon> couponList;
     private TextView textViewMakeCoupon;
     private LinearLayout layoutSavingCoupon;
@@ -55,31 +60,35 @@ public class CouponListActivity extends AppCompatActivity {
 
         couponList = new ArrayList<>();
 
-        ListView usingCouponList = findViewById(R.id.list_using_coupon);
+        final ListView usingCouponList = findViewById(R.id.list_using_coupon);
         couponListAdapter = new CouponListAdapter(this, R.layout.view_coupon, couponList);
         usingCouponList.setAdapter(couponListAdapter);
 
         findViewById(R.id.btn_saving_coupon).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                showQRCodeDialog(savingCoupon);
             }
         });
 
         textViewMakeCoupon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String param = "userIdx=" + user.getIdx() + "&shopIdx=" + shop.getIdx() + "&name=" + shop.getName()+" 적립식 쿠폰" + "&description=적립식 쿠폰" + "&type=S&requestCode=2";
+                String param = "userIdx=" + user.getIdx() + "&shopIdx=" + shop.getIdx() + "&name=" + shop.getName() + " 적립식 쿠폰" + "&description=적립식 쿠폰" + "&type=S&requestCode=2";
 
                 HttpConnect httpConnect = new HttpConnect(param, Constant.SERVER_MAKECOUPON_ADDRESS, requestCallback);
                 httpConnect.execute();
             }
         });
 
-        String param = "userIdx=" + user.getIdx() + "&shopIdx=" + shop.getIdx() + "&requestCode=1";
+        usingCouponList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                showQRCodeDialog(couponList.get(position));
+            }
+        });
 
-        HttpConnect httpConnect = new HttpConnect(param, Constant.SERVER_GETCOUPONLIST_ADDRESS, requestCallback);
-        httpConnect.execute();
+        getCouponListByServer();
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -110,48 +119,73 @@ public class CouponListActivity extends AppCompatActivity {
             try {
                 JSONObject jsonObject = new JSONObject(jsonResult);
 
-                if(jsonObject.getInt("requestCode") == 1){
-                    if(jsonObject.getString("result").equals("ok")){
-                        int savingCount = jsonObject.getInt("saving_count");
+                if (jsonObject.getString("result").equals("ok")) {
+                    int savingCount = jsonObject.getInt("saving_count");
 
-                        if(savingCount > -1){
-                            textViewMakeCoupon.setVisibility(View.GONE);
-                            layoutSavingCoupon.setVisibility(View.VISIBLE);
-
-                            for(int i=0; i<10; i++){
-                                ImageView imgSaving = findViewById(savingCouponIds[i]);
-
-                                if(i < savingCount){
-                                    imgSaving.setAlpha(1.0f);
-                                    imgSaving.startAnimation(AnimationUtils.loadAnimation(CouponListActivity.this, R.anim.bounce));
-                                } else {
-                                    imgSaving.setAlpha(0.3f);
-                                }
-                            }
-                        }
-
-                        couponList.clear();
-
-                        int usingCouponCount = jsonObject.getInt("using_coupon_count");
-                        JSONArray usingCouponArray = jsonObject.getJSONArray("using_coupon_list");
-
-                        for(int i=0; i<usingCouponCount; i++){
-                            couponList.add(new Coupon(usingCouponArray.getJSONObject(i)));
-                        }
-
-                        couponListAdapter.notifyDataSetChanged();
-                    }
-                } else {
-                    if(jsonObject.getString("result").equals("ok")){
+                    if (savingCount > -1) {
                         textViewMakeCoupon.setVisibility(View.GONE);
                         layoutSavingCoupon.setVisibility(View.VISIBLE);
 
+                        for (int i = 0; i < 10; i++) {
+                            ImageView imgSaving = findViewById(savingCouponIds[i]);
+
+                            if (i < savingCount) {
+                                imgSaving.setAlpha(1.0f);
+                                imgSaving.startAnimation(AnimationUtils.loadAnimation(CouponListActivity.this, R.anim.bounce));
+                            } else {
+                                imgSaving.setAlpha(0.3f);
+                            }
+                        }
+
+                        savingCoupon = new Coupon(jsonObject.getJSONObject("saving_coupon_data"));
                     }
+
+                    couponList.clear();
+
+                    int usingCouponCount = jsonObject.getInt("using_coupon_count");
+                    JSONArray usingCouponArray = jsonObject.getJSONArray("using_coupon_list");
+
+                    for (int i = 0; i < usingCouponCount; i++) {
+                        couponList.add(new Coupon(usingCouponArray.getJSONObject(i)));
+                    }
+
+                    couponListAdapter.notifyDataSetChanged();
                 }
+
 
             } catch (JSONException e) {
                 e.printStackTrace();
             }
         }
     };
+
+    private void showQRCodeDialog(Coupon coupon) {
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialog_qr);
+
+        TextView tv = dialog.findViewById(R.id.text_shop_name);
+        tv.setText(coupon.getName());
+
+        ImageView iv = dialog.findViewById(R.id.img_qr);
+        String param = "{\"user_idx\":" + user.getIdx() + ", \"shop_idx\":" + shop.getIdx() + ", \"coupon_idx\":" + coupon.getIdx() + ", \"coupon_name\":\"" + coupon.getName() + "\", \"coupon_desc\":\"" + coupon.getDescription() + "\", \"coupon_type\":\"" + coupon.getType() + "\"}";
+        String url = "https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl=" + param + "&choe=UTF-8";
+        Glide.with(CouponListActivity.this).load(url).into(iv);
+
+        dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialog) {
+                getCouponListByServer();
+            }
+        });
+
+        dialog.show();
+    }
+
+    private void getCouponListByServer() {
+        String param = "userIdx=" + user.getIdx() + "&shopIdx=" + shop.getIdx();
+
+        HttpConnect httpConnect = new HttpConnect(param, Constant.SERVER_GETCOUPONLIST_ADDRESS, requestCallback);
+        httpConnect.execute();
+    }
+
 }
